a377418a9064b4e05dc5692ad7a780e6e1488440
Nguyen Minh Duc <nguyenminhduc1803@gmail.com>
2017-02-19 20:25:04 +0700

    login and logout success

--- js/authentication.js
+++ js/authentication.js
@@ -1,22 +1,67 @@
-
 $(document).ready(function(){
+    /*localStorage.removeItem('result');*/
+    /*localStorage.clear();*/
+    var response = JSON.parse(localStorage.getItem('result'));
+    if (response != null) {
+        /*$('div#login').attr('id','login-new');
+        $('#login-new').html("<div> "+ localStorage.user_name + " </div>");*/
+        /*alert("bbbb");*/
+        $('#login').addClass('hidden');
+        $('.login-new').removeClass('hidden');
+        $('.start-market').removeClass('hidden');
+        $('.user-name').prepend(response.user_name);
+    }
+    $('#logout, #profile').click(function () {
+        if (this.id == 'logout') {
+            $.ajax({
+                type: "POST",
+                url: "http://localhost/trada-backend/index.php/user/logout",
+                success : function(result){
+                    localStorage.removeItem('result');
+                    $('#login').removeClass('hidden');
+                    $('.login-new').addClass('hidden');
+                    $('.start-market').addClass('hidden');
+                    window.location.reload();
+                }
+            });
+            
+        }
+        else if (this.id == 'profile') {
+            alert('MADAFAKA');
+        }
+    });
+
     $("#logsubmit").click(function(){
         var email = $("#email").val();
         var password = $("#password").val();
         var dataString = "user_name=" + email + "&password=" + password;
-        // alert("abc");
-
         $.ajax({
             type: "POST",
             url: "http://localhost/trada-backend/index.php/user/login",
             data: dataString,
             success : function(result){
-                var response = $.parseJSON(result);
-                /*alert (response.email);*/
-                if (response.is_logged_in == 1) {
-                    $('#login').replaceWith("<span> Welcome "+ response.user_name + " </span>");
+                var result1 = JSON.parse(result);
+                if($.isEmptyObject(result1)){
+                    alert("input ngu vai loz");
+                }
+                else{
+                    localStorage.setItem('result',result);
+                    window.location.reload();
                 }
             }
         });
     });
 });
+
+$(document).ready(function(){
+    $('#sign-up').click(function(event){
+        $('#main-content').load('signup.html');
+        event.preventDefault();
+    })
+})
+
+$(document).ready(function(){
+    $('#log-out').click(function(){
+
+    })
+})
